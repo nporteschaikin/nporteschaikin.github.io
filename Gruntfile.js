d79d7c0ff7d38b013bf1a3ddc224a2c84a2989f1
Noah Portes Chaikin <noah@shoptiques.com>
2015-06-25 16:38:40 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    jade: {
+      compile: {
+        files: {
+          'index.html': 'src/index.jade'
+        }
+      }
+    },
+
+    less: {
+      compile: {
+        files: {
+          'css/application.css': ['src/less/application.less']
+        }
+      }
+    },
+
+    postcss: {
+      compile: {
+        options: {
+          processors: [
+            require('autoprefixer-core')()
+          ]
+        },
+        files: {
+          'css/application.css': ['css/application.css']
+        }
+      }
+    },
+
+    watch: {
+      src: {
+        files: 'src/**/*',
+        tasks: 'compile',
+        livereload: true
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+
+  grunt.registerTask('compile', ['jade', 'less', 'postcss']);
+
+}
